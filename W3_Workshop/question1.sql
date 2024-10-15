ae4be0acd768f7fb31d464f83dadc07b4852886a
Vanessa Guerrero <vguerrero@appdev.yearup.org>
2024-10-15 01:07:59 -0500

    Completed question 1

--- W3_Workshop/question1.sql
+++ W3_Workshop/question1.sql
@@ -2,3 +2,12 @@ USE sakila;
 
 /* 1. Display the first and last name of each actor in a single column in upper case letters. 
 	  Name the column in the results Actor Name. */
+      
+SELECT ucase(concat(first_name, ' ', last_name)) AS 'Actor Name'
+FROM actor;
+
+-- Create an additional query to sort Actor Name by the actor’s last name.
+
+SELECT ucase(concat(first_name, ' ', last_name)) AS 'Actor Name'
+FROM actor
+ORDER BY last_name;
